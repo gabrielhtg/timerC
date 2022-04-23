d48e73ac8dfc2a50708a96a64e433e46ca4a4e40
gabrielhtg <gabrielhutagalung970@gmail.com>
2022-04-23 19:43:08 +0700

    baru

--- timer.c
+++ timer.c
@@ -4,51 +4,53 @@
 #else
 #include <unistd.h>
 #endif
+#include <stdlib.h>
 
 int main () {
 	int jj, mm, dd;
-	int sama;
-	int initial = 1;
-	
-	printf("Masukkan waktunya (formatnya jam menit detik) : ");
+
+	printf("Masukkan waktunya (format jam<spasi>menit<spasi>detik): ");
 	scanf("%d %d %d", &jj, &mm, &dd);
 
-	if (jj > 12 || mm > 60 || dd > 60 || jj < 0 || mm < 0 || dd < 0) {
-		printf ("Input yang kamu masukkan tidak valid.\n");
+	if ((jj > 12) || (mm > 60) || (dd > 60) || (jj < 0) || (mm < 0) || (dd < 0)) {
+		printf("Input yang kamu masukkan tidak valid. \n");
 	}
-
-	while (initial == 1) {
-		dd--;
-
-		if (dd < 0 && mm != 0) {
-			dd = 59;
-			mm--;
-		}
-
-		if (dd < 0 && mm >= 0 && jj != 0) {
-			dd = 59;
-			mm = 59;
-			jj--;
-		}
 	
-		if (dd == 0 && mm == 0 && jj == 0) {
-			printf("Time's up.\n");
-			break;
-		}
+	else {
+		while (1) {
+			dd--;
 
-		system("clear");
-		printf("%02d:%02d:%02d\n", jj, mm, dd);
-		sleep(1);
-		system("clear");
+			if (dd < 0 && mm != 0) {
+				dd = 59;
+				mm --;
+			}
+
+			if (dd < 0 && mm >= 0 && jj != 0) {
+				dd = 59;
+				mm = 59;
+				jj--;
+			}
+
+			if (dd == 0 && mm == 0 && jj == 0) {
+				break;
+			} 
+
+			system("clear");
+			printf("%02d:%02d:%02d\n", jj, mm, dd);
+			sleep(1);
+			system("clear");
+		}	
+
+		system("notify-send GTimer \"Time's up.\"");
+		printf("Tekan Ctrl+c untuk mematikan timer.\n\n");
+		system("mpg123 sound.mp3 && clear");
+		printf("--------------------------------------------------------------\n");
+		printf("Terima kasih telah menggunakan.\n");
+		printf("--------------------------------------------------------------\n");
+		printf("Made with love by : Gabriel Cesar Hutagalung\n");
+		printf("--------------------------------------------------------------\n");
 
 	}
-
-	system("notify-send GTimer \"Time's up\"");
-
-	while (1) {
-		system("mpg123 sound.mp3");
-		system("clear");
-	}	
 
 	return 0;
 }

8ad01e63e868522be0997ddd74326e9b494f0f91
gabrielhtg <gabrielhutagalung970@gmail.com>
2022-04-25 20:43:04 +0700

    bug fix

--- timer.c
+++ timer.c
@@ -3,48 +3,48 @@
 #include <stdlib.h>
 
 int main () {
-	int jj, mm, dd;
+	int jam, menit, detik;
 
 	printf("Masukkan waktunya (format jam<spasi>menit<spasi>detik): ");
-	scanf("%d %d %d", &jj, &mm, &dd);
+	scanf("%d %d %d", &jam, &menit, &detik);
 
-	if ((jj > 12) || (mm > 60) || (dd > 60) || (jj < 0) || (mm < 0) || (dd < 0)) {
+	if ((jam > 12) || (menit > 60) || (detik > 60) || (jam < 0) || (menit < 0) || (detik < 0)) {
 		printf("Input yang kamu masukkan tidak valid. \n");
 	}
 	
 	else {
 		while (1) {
-			dd--;
-
-			if (dd < 0 && mm != 0) {
-				dd = 59;
-				mm --;
+			if (detik < 0 && menit != 0) {
+				detik = 59;
+				menit --;
 			}
 
-			if (dd < 0 && mm >= 0 && jj != 0) {
-				dd = 59;
-				mm = 59;
-				jj--;
+			if (detik < 0 && menit >= 0 && jam != 0) {
+				detik = 59;
+				menit = 59;
+				jam--;
 			}
 
-			if (dd == 0 && mm == 0 && jj == 0) {
+			if (detik == 0 && menit == 0 && jam == 0) {
 				break;
 			} 
 
 			system("clear");
-			printf("%02d:%02d:%02d\n", jj, mm, dd);
+			printf("				%02d:%02d:%02d\n", jam, menit, detik);
 			sleep(1);
 			system("clear");
+
+			detik--;
 		}	
 
 		system("notify-send GTimer \"Time's up.\"");
 		printf("Tekan Ctrl+c untuk mematikan timer.\n\n");
 		system("mpg123 sound.mp3 && clear");
-		printf("--------------------------------------------------------------\n");
+		printf("--------------------------------------------------\n");
 		printf("Terima kasih telah menggunakan.\n");
-		printf("--------------------------------------------------------------\n");
+		printf("--------------------------------------------------\n");
 		printf("Made with love by : Gabriel Cesar Hutagalung\n");
-		printf("--------------------------------------------------------------\n");
+		printf("--------------------------------------------------\n");
 
 	}
 
